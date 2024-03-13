af11f1f73bd9fabf03a0f10bc597995fa43c471e
Neriya Malachi <neriamalachi@gmail.com>
2024-03-13 16:21:22 +0200

    finish comits

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isPort= process.env.NODE_ENV === 'production'
 const nextConfig = {
-
-    output:'export',
-    distDir:'dist',
-    images:{
-        unoptimized:true,
-    },
+  basePath: isPort ? "/CardDetails" : "",
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
